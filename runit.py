868fdbe39c5bd8e7342fb06b7ad3e6a005968e54
Klas Modin <klas.modin@gmail.com>
2018-09-05 13:14:50 +0200

    runit file as recieved by Stephen.

--- runit.py
+++ runit.py
@@ -10,26 +10,31 @@ def run():
 	n = 5000
 
 	# Circle
-	t=np.linspace(0,2*np.pi,n,endpoint=False)
-	gamma1 = np.vstack((np.cos(t),np.sin(t))).T
+	#t=np.linspace(0,2*np.pi,n,endpoint=False)
+	#gamma1 = np.vstack((np.cos(t),np.sin(t))).T
 
 	# Ellipse
 	#t=np.linspace(0,2*np.pi,n,endpoint=False)
 	#gamma1 = np.vstack((0.7*np.cos(t),0.9*np.sin(t))).T
 	
 	# Vertical line
-	#t=np.linspace(-1,1,n,endpoint=True)
-	#gamma1 = np.vstack((0.0*t-0.5,t)).T
+	t=np.linspace(-1,1,n,endpoint=True)
+	gamma1 = np.vstack((0.0*t-0.5,t)).T
 	
+	# Circle only in the lower (left-hand) triangle to compare with the matlab code
+	#t=np.linspace(0,2*np.pi,n,endpoint=False)
+	#gamma1 = np.vstack((-0.5+0.25*np.cos(t),-0.5+0.25*np.sin(t))).T
+
 	#pl.figure()
 	#pl.plot(gamma1[:,0],gamma1[:,1])
 	#pl.axis('equal')
 	
-	for l in range(5,22,5):
+	#for l in range(5,22,5):
+	for o in [1]: #range(1,8):
 		#shapecalc = fem.FEMShapeInvariant(order=1, meshsize=4,L=3)
-		shapecalc = fem.FEMShapeInvariant(order=1, meshsize=4*l,L=1.1)
-		inv1 = shapecalc.compute_invariants(gamma1,closed=True)
-		#inv1 = shapecalc.compute_invariants(gamma1,closed=False)
+		shapecalc = fem.FEMShapeInvariant(order=o, meshsize=8,L=2)
+		#inv1 = shapecalc.compute_invariants(gamma1,closed=True)
+		inv1 = shapecalc.compute_invariants(gamma1,closed=False)
 		shapecalc.calcM()
 	
 
